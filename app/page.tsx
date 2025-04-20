f05e4d62be1c40d861b0c35280099876e0b74919
sandro <sandro.jaya@gmail.com>
2025-04-20 12:50:59 +0200

    Add tailwindcss and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
 }
