5d7bc0902f336dc6e97650e0ed60239765d9d267
sandro <sandro.jaya@gmail.com>
2025-04-25 18:02:53 +0200

    cleanup

--- components/boxes/Header.tsx
+++ components/boxes/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PlusIcon, Search } from "lucide-react";
+import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
